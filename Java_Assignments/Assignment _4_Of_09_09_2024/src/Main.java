62d48f45480529c1394d154f5c0ed6d35d2b152c
riteshwadhwani <riteshwadhwani05@gmail.com>
2024-09-10 11:52:08 +0530

    Assignment

---
+++ Java_Assignments/Assignment _4_Of_09_09_2024/src/Main.java
@@ -0,0 +1,48 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+//Question 1
+//		int a = 10000;
+//		System.out.println(a);
+//		double b = (double) a;
+		
+		////////////////////////
+//Question 2
+//		
+//		double c = 100.3334;
+//		float  f = (float) c;
+//		System.out.println(b);
+//		System.out.println(f);
+//		long l = 100;
+//		float f1 = l;
+		
+///////////////////////////////////////////////////
+		//Question 3
+//		double a = 123.23;
+//		float f = 112.3f;
+//		int n = 193;
+//		System.out.println(f+a);
+//		System.out.println(f+n);
+//		System.out.println(a+n);
+		
+//////////////////////////////////////////////////
+//Question 4
+		int a = 1223;
+		double d1 = (double) a;
+		float f = (float) a;
+		String s = Integer.toString(a);
+		System.out.println(d1 + " " + f + " " + s);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
